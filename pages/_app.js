4e1eff1a4563bac7298ef13d093557cefeaaa5bd
lloret82 <adbmanchester@gmail.com>
2022-01-28 22:18:20 +0000

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
 import { UserProvider } from '@auth0/nextjs-auth0'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
+
 import '../styles/globals.css'
 
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
 
 
