ca99af5b6ca8d96bb4283537344f44f95a60ac7e
lloret82 <adbmanchester@gmail.com>
2022-01-28 17:52:36 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { UserProvider } from '@auth0/nextjs-auth0'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+
+
+    <UserProvider>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </UserProvider>
+
+  )
 }
 
-export default MyApp
+
